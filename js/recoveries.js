40010f0a2eb099e72060b0c197c835f90724e1dd
jacquesdebar <jacques.debar@gmail.com>
2020-03-12 06:59:41 -0700

    d3.json not d3.csv

--- js/recoveries.js
+++ js/recoveries.js
@@ -58,7 +58,7 @@ start();
 function start() {
   d3.json("data/recoveries.json", function(data) {
     let headerArray = d3.keys(data[0]);
-    console.log(d3.keys(data[0]));
+    // console.log(d3.keys(data[0]));
     days = headerArray.slice(4, headerArray.length - 1);
 
     d3.select("#dayMenu")
